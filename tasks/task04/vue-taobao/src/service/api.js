988d1c919fffed395df7b2cbb3512899e1bc7f59
torvalds <lyh6f2qo@duck.com>
2022-10-17 11:13:03 +0800

    fix 本地json获取问题
    如果直接连不上服务器，是被 catch 捕获的

--- tasks/task04/vue-taobao/src/service/api.js
+++ tasks/task04/vue-taobao/src/service/api.js
@@ -1,18 +1,22 @@
 import axios from 'axios'
 // cspell:words skus
 
-export async function getLists(){
+export async function getLists() {
     try {
-        const {data} = await axios.get('/api/sku/list')
-        if(data.stat === "OK")
+        const { data } = await axios.get('/api/sku/list')
+        if (data.stat === "OK")
             return data.skus
         else {
-            console.log('request error! use local json file instead :(')
-            const fileContext = await axios.get('./list.json')
-            return fileContext.data.data.skus
+            return localList()
         }
     } catch (error) {
         console.log(error)
-        return []
+        return localList()
     }
+}
+
+async function localList() {
+    console.log('request error! use local json file instead :(')
+    const fileContext = await axios.get('./list.json')
+    return fileContext.data.data.skus
 }
