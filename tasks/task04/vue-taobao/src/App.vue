c503106d9e1f0e7034c85f820134dd742338d642
torvalds <lyh6f2qo@duck.com>
2022-10-17 09:46:48 +0800

    init vue-taobaoList

---
+++ tasks/task04/vue-taobao/src/App.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+div
+  a(href="https://vitejs.dev" target="_blank")
+    img.logo(src="/vite.svg" alt="Vite logo")
+  a(href="https://vuejs.org/" target="_blank")
+    img.logo.vue(src="./assets/vue.svg" alt="Vue logo")
+HelloWorld(msg="Vite + Vue")
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    components: {
+        HelloWorld
+    }
+}
+
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
