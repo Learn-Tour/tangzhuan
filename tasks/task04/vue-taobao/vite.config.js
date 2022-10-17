973f35788005a1cdc9b183bae3060f3c8af894c2
torvalds <lyh6f2qo@duck.com>
2022-10-17 10:24:10 +0800

    add pug sass 等依赖

--- tasks/task04/vue-taobao/vite.config.js
+++ tasks/task04/vue-taobao/vite.config.js
@@ -3,5 +3,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server:{
+    proxy: {
+        // https://learn.91tangzhuan.com/demoapi/sku/list
+        // cspell:words demoapi
+        '/api': {
+            target: 'https://learn.91tangzhuan.com',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, '/demoapi/')
+          },
+    }
+  }
 })
