e44a3d4efd9a316054da7fc4f66dc357ef927c7b
torvalds <lyh6f2qo@duck.com>
2022-10-17 11:15:12 +0800

    fix 静态资源路径问题
    使用相对路径而不是绝对路径，因为不是在根目录

--- tasks/task04/README.md
+++ tasks/task04/README.md
@@ -5,3 +5,7 @@
     - [x] 使用 scss、pug等预处理工具
     - [x] api 通过正则替换 demoapi 为 api
     - [x] 使用 normalize.css 统一重置样式
+- [vue-taobao](./vue-taobao)
+  - 使用 vue 重新实现淘宝列表
+    - [😁 在线预览](https://learn-tour.github.io/tangzhuan/tasks/task04/vue-taobao/dist/)
+    - [x] 服务器失效使用本地 list
--- tasks/task04/vue-taobao/vite.config.js
+++ tasks/task04/vue-taobao/vite.config.js
@@ -3,16 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server:{
-    proxy: {
-        // https://learn.91tangzhuan.com/demoapi/sku/list
-        // cspell:words demoapi
-        '/api': {
-            target: 'https://learn.91tangzhuan.com',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, '/demoapi/')
-          },
+    base: "./",
+    plugins: [vue()],
+    server: {
+        proxy: {
+            // https://learn.91tangzhuan.com/demoapi/sku/list
+            // cspell:words demoapi
+            '/api': {
+                target: 'https://learn.91tangzhuan.com',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '/demoapi/')
+            },
+        }
     }
-  }
 })
